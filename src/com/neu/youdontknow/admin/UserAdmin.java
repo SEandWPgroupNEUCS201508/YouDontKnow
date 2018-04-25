9ebda987f29a5e12c3fd369a188023a7ad3d8b39
HallWoodZhang <hallwood_zhang@163.com>
2018-04-25 15:46:16 +0800

    fix some bug of global utils

--- src/com/neu/youdontknow/admin/UserAdmin.java
+++ src/com/neu/youdontknow/admin/UserAdmin.java
@@ -2,6 +2,7 @@ package com.neu.youdontknow.admin;
 
 import com.neu.youdontknow.models.User;
 import com.neu.youdontknow.utils.DataBaseUtils;
+import com.neu.youdontknow.utils.GlobalUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
@@ -13,7 +14,8 @@ public class UserAdmin {
     public int addUser(User user) throws SQLException {
         QueryRunner queryRunner = new QueryRunner(DataBaseUtils.getDataSource());
         String sql = "insert into user(username, password, email) values(?, ?, ?)";
-        return queryRunner.update(sql, user.getUsername(), user.getPassword(), user.getEmail());
+        return queryRunner.update(sql, user.getUsername(), GlobalUtils.encryptPassword(user.getPassword()),
+                user.getEmail());
     }
 
     public int deleteById(int userId) throws SQLException {
